f2a8032c952a154d5c0e6a729c3035d055ecdf4e
Divyanshu Krishna <divyanshukrishna11@gmail.com>
2023-11-03 22:00:49 +0530

    leetcode

--- valid_palindrome_string.java
+++ valid_palindrome_string.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-   s = s.toLowerCase();
+        s = s.toLowerCase();
         s = s.replaceAll("[^a-z0-9]", "");
 
         return s.equals(new StringBuilder(s).reverse().toString());
-}
+    }
 }
