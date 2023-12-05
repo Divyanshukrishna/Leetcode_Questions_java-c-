4099e07a06fbd745c792179dada6167aef1ffee4
DIVYANSHU_KRISHNA <131163849+Divyanshukrishna@users.noreply.github.com>
2023-12-05 14:00:30 +0530

    contains_duplicate_code

---
+++ contains_duplicate.java
@@ -0,0 +1,9 @@
+    public boolean containsDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length-i;j++){
+                if(nums[i] == nums[j])
+                return true;
+            }
+        }
+        return false;
+    }
